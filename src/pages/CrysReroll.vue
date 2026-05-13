ed87cb42433fc1d5edae89138dad449cf4fe4a1e
TFF <camilla.jahr@hotmail.com>
2026-05-13 11:54:28 +0200

    Add link to stats spreadsheet in CrysReroll.vue

--- src/pages/CrysReroll.vue
+++ src/pages/CrysReroll.vue
@@ -2,7 +2,8 @@
     <div class="add-user">
         <h2>Crystalis Reroller</h2>
         <p>This tool helps automate Crystalis rerolling. Download it <a
-                href="https://github.com/thefrozenfishy/exedra-crys-reroller">here</a></p>
+                href="https://github.com/thefrozenfishy/exedra-crys-reroller">here</a>
+            , and see the stats <a href="https://docs.google.com/spreadsheets/d/1EfElTMXvO9lhbX_KAHAiAHZJ3bzjVye82a2VChNeln0/edit?gid=1422117331#gid=1422117331">here</a></p>
     </div>
 </template>
 
