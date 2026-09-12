dcb193bcf8cbcc96547c5754ae66820d6cdf1caa
TFF <camilla.jahr@hotmail.com>
2026-09-12 20:30:06 +0200

    increase limit now that more runs are displayed in results

--- src/models/BestTeamCalculator.ts
+++ src/models/BestTeamCalculator.ts
@@ -8,7 +8,7 @@ import { KiokuElement, KiokuRole, SupportKey, Aliment } from "../types/enums";
 
 const cache = new Map<string, ScoreAttackKioku>();
 const customPriorityComparator = (a: any[], b: any[]) => a[0] - b[0];
-const LIMIT = 1000;
+const LIMIT = 50_000;
 
 interface KiokuGeneratorArgs {
     name: string;
