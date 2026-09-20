dc69d4da7b886180d6a3a09cd0a332aacb6d97a2
TFF <camilla.jahr@hotmail.com>
2026-09-20 12:34:41 +0200

    enforce type on import too

--- src/store/characterStore.ts
+++ src/store/characterStore.ts
@@ -233,18 +233,14 @@ export const useCharacterStore = defineStore('characterStore', () => {
                 if (!char) return
 
                 Object.assign(char, {
-                    enabled: row.enabled,
-
-                    dupes: row.dupes,
-                    ascension: row.ascension,
-
-                    kiokuLvl: row.kioku_lvl,
-                    magicLvl: row.magic_lvl,
-                    heartphialLvl: row.heartphial_lvl,
-                    specialLvl: row.special_lvl,
-
-                    portrait: row.portrait,
-
+                    enabled: row.enabled ?? false,
+                    dupes: row.dupes ?? 0,
+                    ascension: row.ascension ?? KiokuConstants.minAscension,
+                    kiokuLvl: row.kioku_lvl ?? KiokuConstants.minKiokuLvl,
+                    magicLvl: row.magic_lvl ?? KiokuConstants.minMagicLvl,
+                    heartphialLvl: row.heartphial_lvl ?? KiokuConstants.minHeartphialLvl,
+                    specialLvl: row.special_lvl ?? KiokuConstants.minSpecialLvl,
+                    portrait: row.portrait ?? "",
                     crysOptions: buildCrysOptions(row.character_id, row.crys_options ?? {}),
                 })
                 if (char.rarity < 5) char.ascension = KiokuConstants.maxAscension;
